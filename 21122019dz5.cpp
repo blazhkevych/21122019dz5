12bc31e6ac107fb5f6c783a9409445144ee8b83b
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2020-07-19 17:59:29 +0300

    Добавьте файлы проекта.

---
+++ 21122019dz5.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	cout << "Задание № 5.\n";
+	cout << "\n";
+	cout << "Написать программу, которая преобразует введенное пользователем количество\n";
+	cout << "дней в количество полных недель и оставшихся дней. Например, пользователь ввел 17 дней,\n";
+	cout << "программа должна вывести на экран 2 недели и 3 дня.\n";
+	cout << "\n";
+	cout << "Решение для задания № 5:\n";
+	cout << "\n";
+	double dni_all, nedel, dni_rest;
+	cout << "Введите количество дней : ";
+	cin >> dni_all; //17
+	nedel = int(dni_all / 7);
+	dni_rest = dni_all - nedel * 7;
+	cout << nedel << " недель(и) и " << dni_rest << " дня(ей)" << endl;
+}
